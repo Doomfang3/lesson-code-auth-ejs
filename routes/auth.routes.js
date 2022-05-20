b35f93d9762252dd95b4b31a01561f1c81da361d
Doomfang3 <saubinmathieu@gmail.com>
2022-05-20 12:11:38 +0200

    Replaces Handlebars with EJS

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,26 +1,26 @@
 // routes/auth.routes.js
 
-const { Router } = require("express");
-const router = new Router();
+const { Router } = require('express')
+const router = new Router()
 
-const bcryptjs = require("bcryptjs");
-const saltRounds = 10;
+const bcryptjs = require('bcryptjs')
+const saltRounds = 10
 
-const User = require("../models/User.model");
+const User = require('../models/User.model')
 
 // GET route ==> to display the signup form to users
-router.get("/signup", (req, res) => res.render("auth/signup"));
+router.get('/signup', (req, res) => res.render('auth/signup'))
 
 // POST route ==> to process form data
-router.post("/signup", (req, res, next) => {
+router.post('/signup', (req, res, next) => {
   // console.log("The form data: ", req.body);
 
-  const { username, email, password } = req.body;
+  const { username, email, password } = req.body
 
   bcryptjs
     .genSalt(saltRounds)
-    .then((salt) => bcryptjs.hash(password, salt))
-    .then((hashedPassword) => {
+    .then(salt => bcryptjs.hash(password, salt))
+    .then(hashedPassword => {
       return User.create({
         // username: username
         username,
@@ -28,16 +28,16 @@ router.post("/signup", (req, res, next) => {
         // passwordHash => this is the key from the User model
         //     ^
         //     |            |--> this is placeholder (how we named returning value from the previous method (.hash()))
-        passwordHash: hashedPassword
-      });
+        passwordHash: hashedPassword,
+      })
     })
-    .then((userFromDB) => {
+    .then(userFromDB => {
       // console.log("Newly created user is: ", userFromDB);
-      res.redirect("/userProfile");
+      res.redirect('/userProfile')
     })
-    .catch((error) => next(error));
-});
+    .catch(error => next(error))
+})
 
-router.get("/userProfile", (req, res) => res.render("users/user-profile"));
+router.get('/userProfile', (req, res) => res.render('users/user-profile'))
 
-module.exports = router;
+module.exports = router
