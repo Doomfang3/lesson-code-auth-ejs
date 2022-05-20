23a18e2246cc1786a8dfeb5ad7d8562ee999c027
Doomfang3 <saubinmathieu@gmail.com>
2022-05-20 12:15:53 +0200

    Replace Handlebars with EJS

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,45 +1,45 @@
 // routes/auth.routes.js
 
-const { Router } = require("express");
-const router = new Router();
+const { Router } = require('express')
+const router = new Router()
 
-const mongoose = require("mongoose"); // <== has to be added
-const bcryptjs = require("bcryptjs");
-const saltRounds = 10;
+const mongoose = require('mongoose') // <== has to be added
+const bcryptjs = require('bcryptjs')
+const saltRounds = 10
 
-const User = require("../models/User.model");
+const User = require('../models/User.model')
 
 // GET route ==> to display the signup form to users
-router.get("/signup", (req, res) => res.render("auth/signup"));
+router.get('/signup', (req, res) => res.render('auth/signup'))
 
 // POST route ==> to process form data
-router.post("/signup", (req, res, next) => {
+router.post('/signup', (req, res, next) => {
   // console.log("The form data: ", req.body);
 
-  const { username, email, password } = req.body;
+  const { username, email, password } = req.body
 
   // make sure users fill all mandatory fields:
   if (!username || !email || !password) {
-    res.render("auth/signup", {
-      errorMessage: "All fields are mandatory. Please provide your username, email and password."
-    });
-    return;
+    res.render('auth/signup', {
+      errorMessage: 'All fields are mandatory. Please provide your username, email and password.',
+    })
+    return
   }
 
   // make sure passwords are strong:
-  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/
   if (!regex.test(password)) {
-    res.status(500).render("auth/signup", {
+    res.status(500).render('auth/signup', {
       errorMessage:
-        "Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter."
-    });
-    return;
+        'Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.',
+    })
+    return
   }
 
   bcryptjs
     .genSalt(saltRounds)
-    .then((salt) => bcryptjs.hash(password, salt))
-    .then((hashedPassword) => {
+    .then(salt => bcryptjs.hash(password, salt))
+    .then(hashedPassword => {
       return User.create({
         // username: username
         username,
@@ -47,26 +47,27 @@ router.post("/signup", (req, res, next) => {
         // passwordHash => this is the key from the User model
         //     ^
         //     |            |--> this is placeholder (how we named returning value from the previous method (.hash()))
-        passwordHash: hashedPassword
-      });
+        passwordHash: hashedPassword,
+      })
     })
-    .then((userFromDB) => {
+    .then(userFromDB => {
       // console.log("Newly created user is: ", userFromDB);
-      res.redirect("/userProfile");
+      res.redirect('/userProfile')
     })
-    .catch((error) => {
+    .catch(error => {
       if (error instanceof mongoose.Error.ValidationError) {
-        res.status(500).render("auth/signup", { errorMessage: error.message });
+        res.status(500).render('auth/signup', { errorMessage: error.message })
       } else if (error.code === 11000) {
-        res.status(500).render("auth/signup", {
-          errorMessage: "Username and email need to be unique. Either username or email is already used."
-        });
+        res.status(500).render('auth/signup', {
+          errorMessage:
+            'Username and email need to be unique. Either username or email is already used.',
+        })
       } else {
-        next(error);
+        next(error)
       }
-    }); // close .catch()
-});
+    }) // close .catch()
+})
 
-router.get("/userProfile", (req, res) => res.render("users/user-profile"));
+router.get('/userProfile', (req, res) => res.render('users/user-profile'))
 
-module.exports = router;
+module.exports = router
